275f012fc565f34cdb6756b59d12af8673577c29
Dima <Lebocski@yandex.ru>
2021-02-21 13:57:04 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut",
+                "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        Random rand = new Random();
+        int task = rand.nextInt(words.length);
+        String quiz = words[task];
+
+        System.out.println("Угадайте слово: ");
+
+//        System.out.println(words[task]);
+        Scanner sc = new Scanner(System.in);
+        String sol;
+
+        do {
+
+            sol = sc.nextLine();
+
+            if (quiz.equals(sol)) {
+                System.out.println("Вы угадали слово: " + quiz + "!");
+            } else {
+                adv(quiz, sol);
+            }
+        }
+        while (!quiz.equals(sol));
+    }
+
+            static void adv (String quiz, String sol) {
+                String answer = "";
+
+                for (int i = 0; i < 15; i++) {
+
+                   if (i >= quiz.length() || i >= sol.length()) {
+                        answer += "#";
+                   }
+                    else if (quiz.charAt(i) == sol.charAt(i)) {
+                        answer += sol.charAt(i);
+                    } else {
+                        answer += "#";
+                    }
+
+            }
+                System.out.println(answer);
+    }
+}
